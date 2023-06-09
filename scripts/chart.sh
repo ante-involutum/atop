bd0e86bd8850ac3c8ab323a7e25508eac2215bb2
lunzhou <lunzhou@yunify.com>
2023-06-10 00:30:45 +0800

    chore: chart

--- scripts/chart.sh
+++ scripts/chart.sh
@@ -1,8 +1,9 @@
 helm dependency update charts
 helm package charts
-mv atop-1.0.0.tgz ../chartrepo 
+mv atop-*.tgz ../chartrepo 
 cd ../chartrepo
 helm repo index --url  https://no8ge.github.io/chartrepo/ .
 git add .
 git commit -m 'updata atop'
 git push
+rm atop-*.tgz
