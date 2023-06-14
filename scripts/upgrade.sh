0fcd7a78e578fea42fc78a821155c4d25bf78618
lunzhou <lunzhou@yunify.com>
2023-06-14 17:22:21 +0800

    feat: add es ilm

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,10 +2,10 @@ atopVersion="1.0.1"
 env=dev
 namespace=top
 
+# stable
 helm repo update
-
 helm upgrade --install  $env  atop/atop --version $atopVersion --create-namespace --namespace $namespace -f values.yaml --set env=$env
  
-
-
-#helm upgrade --install apisix atop/apisix --create-namespace --namespace apisix --set gateway.http.nodePort=31690 --set dashboard.enabled=true --set ingress-controller.enabled=true --set ingress-controller.config.apisix.serviceNamespace=apisixg
+# debug
+helm upgrade --install apisix atop/apisix --create-namespace --namespace apisix --set gateway.http.nodePort=31690 --set dashboard.enabled=true --set ingress-controller.enabled=true --set ingress-controller.config.apisix.serviceNamespace=apisix
+helm upgrade --install  dev  charts --create-namespace --namespace top 
